a3c28f417fe0d3854cc68a0ac07615a234e78062
Eddie Xu <eddie.xu.apple@gmail.com>
2023-03-12 03:42:55 -0400

    uploaded json and xml file

--- fav_book.sql
+++ fav_book.sql
@@ -1,3 +1,4 @@
+-- create a table for favorite books
 CREATE TABLE fav_book(
 	book_id INT,
 	book_title VARCHAR,
@@ -7,6 +8,7 @@ CREATE TABLE fav_book(
 	book_price FLOAT
 );
 
+-- insert values to the fav_book database
 INSERT INTO fav_book (book_id, book_title, book_author, book_genre, book_lang, book_price) 
 VALUES (1,'Pride and Prejudice', 'Jane Austen', 'Romance', 'English', 7.99),
 (2,'The Da Vinci Code', 'Dan Brown', 'Mystery Thriller', 'English', 20.73),
@@ -14,3 +16,8 @@ VALUES (1,'Pride and Prejudice', 'Jane Austen', 'Romance', 'English', 7.99),
 
 SELECT * FROM fav_book;
 
+-- export the table to json text file.
+COPY (
+	SELECT row_to_json(fav_book) :: text
+	FROM fav_book
+) TO 'computerlink';
